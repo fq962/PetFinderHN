ae21edaa3aebc68d255a63ea245a23d085145f89
fq962 <75685082+fq962@users.noreply.github.com>
2022-03-15 22:08:52 -0600

    pantalla de login

---
+++ lib/widgets/appbar.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class _appbar extends StatelessWidget {
+@override
+  Widget build(BuildContext context) {
+  return AppBar(
+    title: const Text('PetFinder'),
+    backgroundColor: const Color.fromRGBO(255, 179, 114, 1),
+  );
+}
+}
